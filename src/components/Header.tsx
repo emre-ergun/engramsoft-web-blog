d49a47af96b211046c2b27f5351d301d13767a2f
Emre Ergun <emreergun@engramsoft.com>
2024-11-14 00:23:04 +0300

    navigation links added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import ThemeSwitch from '@/src/components/ThemeSwitch';
 import SvgLogo from '@/src/components/SvgLogo';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -8,7 +9,17 @@ function Header() {
         <SvgLogo style='w-[3rem] h-[3rem] fill-black dark:fill-white' />
         <h1 className='text-3xl font-bold'>EngramSoft's Blog</h1>
       </div>
-      <ThemeSwitch />
+      <div className='flex flex-col md:flex-row items-center gap-0 md:gap-4 text-lg'>
+        <Link to={'/'} className='hover:text-violet-500'>
+          Home
+        </Link>
+        <Link to={'/about'} className='hover:text-violet-500'>
+          About
+        </Link>
+        <div className='hidden md:block'>
+          <ThemeSwitch />
+        </div>
+      </div>
     </div>
   );
 }
