bde6fb694943986c4d8a3a2a7509ec2d58e1f8b2
Emre Ergun <emreergun@engramsoft.com>
2024-11-14 02:37:20 +0300

    Link component changed to NavLink to use active feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import SvgLogo from '@/src/components/SvgLogo';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { RxCross1, RxHamburgerMenu } from 'react-icons/rx';
 import { useState } from 'react';
 
@@ -7,22 +7,25 @@ function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='w-full flex justify-between items-center min-h-[64px]'>
-      <Link to='/' className='flex items-center gap-2'>
+    <div className='w-full flex justify-between items-end min-h-[48px]'>
+      <Link to='/' className='flex items-end gap-2'>
         <SvgLogo style='w-[3rem] h-[3rem] fill-black' />
         <h1 className='text-3xl font-bold'>EngramSoft's Blog</h1>
       </Link>
-      <div className='flex items-center justify-center gap-4'>
-        <div className='hidden sm:flex items-center gap-4 text-xl'>
-          <Link to={'/'} className='hover:text-violet-500'>
+      <div className='flex items-end justify-center gap-4'>
+        <div className='hidden sm:flex items-end gap-4 text-xl'>
+          <NavLink to={'/'} className=' hover:text-violet-500'>
             Home
-          </Link>
-          <Link to={'/about'} className='hover:text-violet-500'>
+          </NavLink>
+          <NavLink to={'/about'} className='hover:text-violet-500'>
             About
-          </Link>
+          </NavLink>
+          <NavLink to={'/sign-in'} className='hover:text-violet-500'>
+            Sign in
+          </NavLink>
         </div>
         <div
-          className='flex sm:hidden items-center justify-center'
+          className='flex sm:hidden items-end justify-center'
           onClick={() => setIsMenuOpen(prev => !prev)}
         >
           {isMenuOpen ? (
@@ -34,20 +37,23 @@ function Header() {
       </div>
       {isMenuOpen && (
         <div className='fixed flex flex-col gap-4 py-6 px-6 border-2 border-black rounded-md top-[64px] right-[8px] text-xl'>
-          <Link
+          <NavLink to={'/sign-in'} className='hover:text-violet-500 hover:underline'>
+            Sign in
+          </NavLink>
+          <NavLink
             to={'/'}
             className='hover:text-violet-500 hover:underline'
             onClick={() => setIsMenuOpen(false)}
           >
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to={'/about'}
             className='hover:text-violet-500 hover:underline'
             onClick={() => setIsMenuOpen(false)}
           >
             About
-          </Link>
+          </NavLink>
         </div>
       )}
     </div>
