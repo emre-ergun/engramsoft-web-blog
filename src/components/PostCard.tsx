60486ef2ac19728c233f92cfa8fe422904669da6
Emre Ergun <emreergun@engramsoft.com>
2024-11-15 20:41:56 +0300

    img added to postcard as an example

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,8 +7,13 @@ type PostCardProps = Post & {
 function PostCard({ title, content, keywords }: PostCardProps) {
   return (
     <div className='group relative flex flex-col bg-fourth shadow-md border border-primary p-2 h-full'>
-      <div className='h-1/2'></div>
-      <div className='flex flex-col h-1/2 justify-end'>
+      <div className='h-2/3 overflow-hidden group-hover:scale-[0.9] group-hover:-rotate-1 group-hover:translate-x-1 transition-all duration-500'>
+        <img
+          src='https://picsum.photos/1920/1080.webp?t=1'
+          className='object-scale-cover w-full'
+        ></img>
+      </div>
+      <div className='flex flex-col h-1/3 justify-end'>
         <h2 className='text-md font-bold line-clamp-1'>{title}</h2>
         <p className='line-clamp-1'>{content}</p>
         <ul className='absolute hidden flex-col items-end top-2 right-2 z-10 gap-2 sm:group-hover:flex'>
