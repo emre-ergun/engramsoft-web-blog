30b232c69b7083ef5821c54e5b736003ccd6383f
Emre Ergun <emreergun@engramsoft.com>
2025-02-21 17:34:20 +0300

    eslint.configs updated to fix some diagnostic informations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainLayout from '@/src/layouts/MainLayout';
-import Home from '@/src/pages/Home';
-import NotFound from '@/src/pages/NotFound';
-import SignIn from '@/src/pages/SignIn';
-import SignUp from '@/src/pages/SignUp';
-import AuthProvider from '@/context/AuthContext';
-import QueryProvider from '@/context/QueryProvider';
-import Post from '@/src/pages/Post';
-import PostEditor from '@/src/pages/PostEditor';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainLayout from "@/src/layouts/MainLayout";
+import Home from "@/src/pages/Home";
+import NotFound from "@/src/pages/NotFound";
+import SignIn from "@/src/pages/SignIn";
+import SignUp from "@/src/pages/SignUp";
+import AuthProvider from "@/context/AuthContext";
+import QueryProvider from "@/context/QueryProvider";
+import Post from "@/src/pages/Post";
+import PostEditor from "@/src/pages/PostEditor";
 
 function App() {
   return (
@@ -17,13 +17,13 @@ function App() {
           future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
         >
           <Routes>
-            <Route path='/' element={<MainLayout />}>
+            <Route path="/" element={<MainLayout />}>
               <Route index element={<Home />} />
-              <Route path='edit' element={<PostEditor />} />
-              <Route path='post/:id/:name' element={<Post />} />
-              <Route path='sign-in' element={<SignIn />} />
-              <Route path='sign-up' element={<SignUp />} />
-              <Route path='*' element={<NotFound />} />
+              <Route path="edit" element={<PostEditor />} />
+              <Route path="post/:id/:name" element={<Post />} />
+              <Route path="sign-in" element={<SignIn />} />
+              <Route path="sign-up" element={<SignUp />} />
+              <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
         </Router>
